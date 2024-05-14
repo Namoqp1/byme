362ac46f860069f5ba8839708e9fa118d564f7ac
Namoqp1 <122848983+Namoqp1@users.noreply.github.com>
2024-05-14 23:31:35 +0700

    Namo.lua

---
+++ MainScript/Goodhub/Namo.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 16644455867 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Namoqp1/config/main/README.md"))()
+elseif game.PlaceId == 11448431235 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Namoqp1/gay-fix/main/README.md"))()
+end
